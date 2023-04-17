8b7e051b1f9691e9ec408c6c3a63f0e97a361e6e
MANASSEH IDEHEN <121174794+Manasseh61@users.noreply.github.com>
2023-04-17 12:52:45 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,10 @@
+let input = document.querySelector('.questions')
+let input = document.querySelector('.questions2')
+let button = document.querySelector('button')
+let result = document.querySelector('result')
+
+buntton.onclick= button.onclick=function() 
+
+
+
+
